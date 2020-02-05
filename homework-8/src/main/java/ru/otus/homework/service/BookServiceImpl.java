ee204198e80cd8ba353985e3b9ff4c87dbcee5e2
Vladimir <vzlobin2003@yandex.ru>
2020-02-05 17:19:54 +0300

    fix. Add DB services

--- homework-8/src/main/java/ru/otus/homework/service/BookServiceImpl.java
+++ homework-8/src/main/java/ru/otus/homework/service/BookServiceImpl.java
@@ -6,28 +6,24 @@ import ru.otus.homework.domain.Author;
 import ru.otus.homework.domain.Book;
 import ru.otus.homework.domain.Comment;
 import ru.otus.homework.domain.Genre;
-import ru.otus.homework.repo.BookRepo;
-import ru.otus.homework.repo.CommentRepo;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
-    private final BookRepo bookRepo;
-    private final SequenceGeneratorService sequenceGeneratorService;
     private final AuthorService authorService;
-    private final CommentRepo commentRepo;
     private final GenreService genreService;
+    private final BookDBService bookDBService;
+    private final CommentDBService commentDBService;
 
 
     @Autowired
-    public BookServiceImpl(BookRepo bookRepo, SequenceGeneratorService sequenceGeneratorService, AuthorService authorService, CommentRepo commentRepo, GenreService genreService) {
-        this.bookRepo = bookRepo;
-        this.sequenceGeneratorService = sequenceGeneratorService;
+    public BookServiceImpl(AuthorService authorService, GenreService genreService, BookDBService bookDBService, CommentDBService commentDBService) {
         this.authorService = authorService;
-        this.commentRepo = commentRepo;
         this.genreService = genreService;
+        this.bookDBService = bookDBService;
+        this.commentDBService = commentDBService;
     }
 
     @Override
@@ -35,64 +31,62 @@ public class BookServiceImpl implements BookService {
         Author author = authorService.getAuthor(authorName);
         Genre genre = genreService.getGenre(genreName);
         Book book = new Book(title, author, genre);
-        book.setId(sequenceGeneratorService.generateSequence(Book.SEQUENCE_NAME));
-        bookRepo.save(book);
+        bookDBService.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return bookRepo.findAll();
+        return bookDBService.findAll();
     }
 
     @Override
     public Book findById(long id) {
-        return bookRepo.findById(id);
+        return bookDBService.findById(id);
     }
 
     @Override
     public void deleteById(long id) {
-        bookRepo.findById(id).getComments().forEach(commentRepo::delete);
-        bookRepo.deleteById(id);
+        bookDBService.findById(id).getComments().forEach(commentDBService::delete);
+        bookDBService.deleteById(id);
     }
 
     @Override
     public long getCount() {
-        return bookRepo.count();
+        return bookDBService.count();
     }
 
     @Override
     public void updateNameById(long id, String name) {
-        Book book = bookRepo.findById(id);
+        Book book = bookDBService.findById(id);
         book.setTitle(name);
-        bookRepo.save(book);
+        bookDBService.save(book);
     }
 
     @Override
     public List<Book> findByName(String name) {
-        return bookRepo.findAllByTitle(name);
+        return bookDBService.findAllByTitle(name);
     }
 
     @Override
     public void addComment(long bookId, String commentText) {
-        Book book = bookRepo.findById(bookId);
+        Book book = bookDBService.findById(bookId);
         if (book != null) {
             Comment comment = new Comment(commentText);
-            comment.setId(sequenceGeneratorService.generateSequence(Comment.SEQUENCE_NAME));
-            commentRepo.save(comment);
+            commentDBService.save(comment);
             book.setComments(addCommentToBookCommentList(book, comment));
-            bookRepo.save(book);
+            bookDBService.save(book);
         }
     }
 
     @Override
     public List<Comment> findCommentsByBookId(long id) {
-        Book book = bookRepo.findById(id);
+        Book book = bookDBService.findById(id);
         return book.getComments();
     }
 
     @Override
     public List<Book> findAllBooksByAuthorId(long id) {
-        return bookRepo.findAllByAuthorId(id);
+        return bookDBService.findAllByAuthorId(id);
     }
 
 
