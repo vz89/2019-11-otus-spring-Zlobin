b3ca65450c4288f710b38b808662738c080267ee
Vladimir <vzlobin2003@yandex.ru>
2020-02-06 20:31:08 +0300

    fix

--- homework-8/src/main/java/ru/otus/homework/service/DBService.java
+++ homework-8/src/main/java/ru/otus/homework/service/DBService.java
@@ -7,21 +7,5 @@ import java.util.List;
 
 
 public interface DBService extends BookDBService, CommentDBService{
-    void save(Book book);
 
-    List<Book> findAll();
-
-    Book findById(long id);
-
-    void delete(Comment comment);
-
-    void deleteById(long id);
-
-    long count();
-
-    List<Book> findAllByTitle(String name);
-
-    void save(Comment comment);
-
-    List<Book> findAllByAuthorId(long id);
 }
