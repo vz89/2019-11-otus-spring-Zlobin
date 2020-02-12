817187b26ce904fc8e512373486b91e20fff70cb
Vladimir <vzlobin2003@yandex.ru>
2020-02-12 22:41:39 +0300

    fix backend

--- homework-10/frontend/src/App.js
+++ homework-10/frontend/src/App.js
@@ -7,14 +7,31 @@ import CreateBookForm from "./form/CreateBookForm";
 import EditBookForm from "./form/EditBookForm";
 
 class App extends Component {
+    state = AppStartState;
 
+    componentDidMount() {
+        Api.getBooks().then((response) => {
+            this.setState({
+                books: response.data
+            })
+        });
+    }
 
+    deleteBook(id) {
+        let Id = Number.parseInt(id);
+        Api.deleteBook(Id).then(() => {
+            this.componentDidMount();
+        });
+    }
 
-
-
-
-
-
+    addBook() {
+        Api.addBook(this.state.newBookData).then(() => {
+            this.setState({
+                newBookData: AppStartState.newBookData
+            });
+            this.componentDidMount();
+        });
+    }
 
     editBook(id) {
         let Id = Number.parseInt(id);
@@ -81,10 +98,63 @@ class App extends Component {
     }
 
     render() {
+        const books = this.state.books.map((book, index) => {
+            return (
+                <Book
+                    id={book.id}
+                    index={index}
+                    title={book.title}
+                    authorname={book.author.name}
+                    genrename={book.genre.name}
+                    editBook={this.toggleEditBookModel.bind(this, book.id, book.title, book.author.name, book.genre.name)}
+                    deleteBook={this.deleteBook.bind(this, book.id)}
+                />
+            )
+        });
         return (
-            <div className="App">
-                <Books/>
+            <div className="App m-3">
+                <h1>Library</h1>
+                <div className={"m-3"}>
+                    <CreateBookForm
+                        id=""
+                        title={this.state.newBookData.title}
+                        authorName={this.state.newBookData.author.name}
+                        genreName={this.state.newBookData.genre.name}
+                        changeTitleFieldHandler={this.changeTitleFieldHandler.bind(this)}
+                        changeAuthorGenreFieldHandler={this.changeAuthorGenreFieldHandler.bind(this)}
+                        addBook={this.addBook.bind(this)}
+                        buttonLabel="Create Book"
+                    />
+                </div>
+                <EditBookForm
+                    editBookForm={this.state.editBookForm}
+                    toggleEditBookModel={this.toggleEditBookModel.bind(this)}
+                    changeTitleFieldHandler={this.changeTitleFieldHandler.bind(this)}
+                    changeAuthorGenreFieldHandler={this.changeAuthorGenreFieldHandler.bind(this)}
+                    id={this.state.editBookData.id}
+                    title={this.state.editBookData.title}
+                    authorName={this.state.editBookData.author.name}
+                    genreName={this.state.editBookData.genre.name}
+                    editBook={this.editBook.bind(this, this.state.editBookData.id)}
+                />
+                <div>
+                    <Table>
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Genre</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {books}
+                        </tbody>
+                    </Table>
+                </div>
             </div>
+
         )
     }
 }
