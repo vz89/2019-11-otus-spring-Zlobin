f20bccb77178356fa9350cdeaf950f1a67ece4a1
Vladimir <vzlobin2003@yandex.ru>
2020-03-29 17:50:07 +0300

    add spring data rest

--- homework-16/src/main/java/ru/otus/homework/repo/BookRepository.java
+++ homework-16/src/main/java/ru/otus/homework/repo/BookRepository.java
@@ -2,10 +2,12 @@ package ru.otus.homework.repo;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.homework.domain.Book;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "books")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @EntityGraph(value = "author_genre_entity_graph")
--- homework-16/src/main/java/ru/otus/homework/repo/CommentRepository.java
+++ homework-16/src/main/java/ru/otus/homework/repo/CommentRepository.java
@@ -1,11 +1,13 @@
 package ru.otus.homework.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.otus.homework.domain.Book;
 import ru.otus.homework.domain.Comment;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "comments")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByBook(Book book);
 }
