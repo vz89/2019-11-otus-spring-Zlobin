ce81f9317f13968c489a6201f06ed6c9bde3a444
Vladimir <vzlobin2003@yandex.ru>
2020-05-09 14:03:12 +0300

    fix clients

--- PROJECT/src/main/java/com/project/holyvacation/client/WeatherRestClientImpl.java
+++ PROJECT/src/main/java/com/project/holyvacation/client/WeatherRestClientImpl.java
@@ -1,25 +1,24 @@
 package com.project.holyvacation.client;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@RequiredArgsConstructor
 public class WeatherRestClientImpl implements WeatherRestClient {
 
     private final RestTemplate restTemplate;
-
-    @Value("${api.weather.serverUrl}")
     private String serverUrl;
-
-    @Value("${api.weather.apiKey}")
     private String apiKey;
+
+    public WeatherRestClientImpl(RestTemplate restTemplate, @Value("${api.weather.serverUrl}") String serverUrl, @Value("${api.weather.apiKey}") String apiKey) {
+        this.restTemplate = restTemplate;
+        this.serverUrl = serverUrl;
+        this.apiKey = apiKey;
+    }
 
     @Override
     public Object getWeatherByLatLon(double lat, double lon) {
-
         return restTemplate.getForObject(serverUrl + "?" + "lat=" + lat + "&" + "lon=" + lon + "&" + "exclude=minutely,hourly" + "&" + "appid=" + apiKey, Object.class);
     }
 
